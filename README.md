56de8c1aeb920ad34b2884c2629cfb647d09d158
Sarah Verbelen <sarah.verbelen@student.ehb.be>
2020-06-10 16:45:04 +0200

    homescherm functionaliteit wip

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * https://angular.io/
 * https://stackoverflow.com
 * https://w3schools.com 
+* https://firebase.google.com/docs/web/
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  currentRoom = 0;
+
+  linkList = ['/livestream', '/faq', '/network'];
+  nameList = ['Livestream', 'Help Center', 'Netwerk'];
+
+  currentLink = '/livestream';
+  currentName = 'Livestream';
+  currentImage = 'assets/images/room0.png';
+
   constructor() { }
+
+  nextRoom(next: boolean) {
+    if (next) { // this just makes sure the arrows move the rooms in a different direction
+      this.currentRoom = (this.currentRoom - 1) % 3;
+    } else {
+      this.currentRoom = (this.currentRoom + 1) % 3;
+    }
+
+
+    let roomNumber = this.currentRoom;
+    if(this.currentRoom < 0){
+      // if it's a negative number, make it positive so you can use it as an index for the linkList and nameList arrays
+      roomNumber = Math.abs(this.currentRoom);
+    }
+    this.currentLink = this.linkList[roomNumber];
+    this.currentName = this.nameList[roomNumber];
+    this.currentImage = 'assets/images/room' + roomNumber + '.png';
+
+  }
 
   ngOnInit(): void {
   }
