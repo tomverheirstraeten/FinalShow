fedd5d1b73dc16460aac1e388df847cf96d2ac01
Sarah Verbelen <sarah.verbelen@student.ehb.be>
2020-06-27 09:49:17 +0200

    production environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,19 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
+   firebaseConfig: { // echte database
     apiKey: 'AIzaSyABvAb09DZzUPPNXyQNuk0-tDaRdoOAqzs',
-    authDomain: 'finalshow.firebaseapp.com',
+    authDomain: 'finalshow.be',
     databaseURL: 'https://finalshow.firebaseio.com',
     projectId: 'finalshow',
     storageBucket: 'finalshow.appspot.com',
     messagingSenderId: '853852160934',
     appId: '1:853852160934:web:fc998d0dc3e790c0270567'
+  },
+  credentials: {
+    username: 'admin',
+    password: 'ehbfinalshowadminpanel'
+  },
+  livestreamID: {
+    id: 'xWsddAUZ7AFFUOVJ9Muv', //echte database
   }
 };
